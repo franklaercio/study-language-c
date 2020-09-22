0b2d1468cabb036d79cce7dd1cee5d15c08d3dfc
franklaercio <franklaercio@gmail.com>
2020-09-21 21:28:09 -0300

    Adding produto_escalar

--- Listas/Semana 3/rotacao_bits.c
+++ Listas/Semana 3/rotacao_bits.c
@@ -3,6 +3,7 @@
 int main() {
     int number, rotacions;
     int bits[8];
+    int results[8];
 
     for (int i = 0; i < 8; i++){
         scanf("%i", &number);
@@ -13,7 +14,15 @@ int main() {
     scanf("%i", &rotacions);
 
     for (int i = 0; i < 8; i++){
-        printf("%i ", bits[i]);
+        if ((i+rotacions) > 4) {
+            results[i-rotacions] = bits[i];
+        }else{
+            results[i+rotacions] = bits[i];
+        }
+    }
+
+    for (int i = 0; i < 8; i++){
+        printf("%i ", results[i]);
     }
 
     return 0;
