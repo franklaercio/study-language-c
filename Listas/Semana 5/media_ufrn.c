ed4b96ece8ac9bdea2eca4098d1a907a5d3f26e1
franklaercio <franklaercio@gmail.com>
2020-10-05 20:17:18 -0300

    Adding media_ufrn.c

---
+++ Listas/Semana 5/media_ufrn.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+float calculaMedia(char tipoMedia, float nota1, float nota2, float nota3){
+    float resultado;
+
+    if(tipoMedia == "A"){
+        resultado = ((nota1*4) + (nota2*5) + (nota3*6))/15;
+    }else if(tipoMedia == "N"){
+        resultado = ((nota1) + (nota2) + (nota3))/3;
+    }
+
+    return resultado;
+}
+
+int main(){
+    char tipoMedia;
+    float nota1;
+    float nota2;
+    float nota3;
+
+    scanf("%c", &tipoMedia);
+    scanf("%f", &nota1);
+    scanf("%f", &nota2);
+    scanf("%f", &nota3);
+
+    printf("%.2f", calculaMedia(tipoMedia, nota1, nota2, nota3));
+
+    return 0;
+}
