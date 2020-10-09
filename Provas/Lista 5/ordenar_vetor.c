e1de9a106a90043259b72a8211b0892c6c147487
franklaercio <franklaercio@gmail.com>
2020-10-09 00:30:10 -0300

    Adding ordernar_vetor.c

--- Provas/Lista 5/ordenar_vetor.c
+++ Provas/Lista 5/ordenar_vetor.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
-int ordenarVetorCrescente(int numeros[5]){
+int imprimirVetor(int numeros[5]){
+    for (int i = 0; i < 5; i++){
+        printf("%d ", numeros[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
+int imprimirOrdemCrescenteDecrescente(int numeros[5]){
     int i, k;
     char numeroAux;
 
-    for (i = strlen(numeros) - 1; i > 0; i--){
+    for (i = 5; i > 0; i--){
         for (k = 0; k < i; k++){
             if(numeros[k] > numeros[k + 1]) {
                 numeroAux = numeros[k];
@@ -14,52 +22,32 @@ int ordenarVetorCrescente(int numeros[5]){
             }
         }
     }
+
+    imprimirVetor(numeros);
+
+    for (i = 4; i >= 0; i--){
+        printf("%d ", numeros[i]);
+    }
+
+    printf("\n");
     
     return numeros[40];
-}
-
-int ordenarVetorDecrescente(int numeros[5]){
-    int i, k;
-    char numeroAux;
-
-    for (i = strlen(numeros) - 1; i > 0; i--){
-        for (k = 0; k < i; k++){
-            if(numeros[k] < numeros[k + 1]) {
-                numeroAux = numeros[k];
-                numeros[k] = numeros[k+1];
-                numeros[k+1] = numeroAux;                     
-            }
-        }
-    }
-    
-    return numeros[40];
-}
-
-void imprimirVetor(int numeros[5]){
-    for (int i = 0; i < 5; i++){
-        printf("%d", numeros[i]);
-    }
 }
 
 int main () {
-    char numeros[5];
+    int numeros[5];
+    int ordemDigitacao[5];
 
     for (int i = 0; i < 5; i++){
-        char numero;
-        scanf("%d", numero);
+        int numero = 0;
+        scanf("%d", &numero);
+        numeros[i] = numero;
+        ordemDigitacao[i] = numero;
     }
-
-    char crescente = strcpy(numeros);
-    int decrescente = strcpy(numeros);
     
-    ordenarVetorCrescente(crescente);
-    ordenarVetorCrescente(decrescente);
-
-
-
-
-
-
+    imprimirOrdemCrescenteDecrescente(numeros);
+    
+    imprimirVetor(ordemDigitacao);
 
     return 0;
 }
