18729813fbe1a4f48fb26138a8ea9e882ea4c0c6
franklaercio <franklaercio@gmail.com>
2020-10-10 22:42:36 -0300

    Adding numero_perfeito.c

--- Provas/Lista 5/numero_perfeito.c
+++ Provas/Lista 5/numero_perfeito.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+int imprimirDivisores(int numero){
+    for(int i=0; i <= numero; i++){
+        if(numero%i == 0){
+            printf("%d", numero);
+        }
+    }
+}
+
 int main () {
+    int numero;
+    scanf("%d", &numero);
+
+    imprimirDivisores(numero);
 
     return 0;
 }
