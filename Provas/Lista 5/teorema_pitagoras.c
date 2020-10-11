ebc6031d1981b3f9d2340da30776e2dacdb9fd3d
franklaercio <franklaercio@gmail.com>
2020-10-11 14:08:02 -0300

    Adding teorema_pitagoras.c

--- Provas/Lista 5/teorema_pitagoras.c
+++ Provas/Lista 5/teorema_pitagoras.c
@@ -14,31 +14,32 @@ int calculaMaximoDivisorComum(int x, int y){
     return y;
 }
 
-bool isTriplaPitagorica(int x, int y, int z){
-  int somaNumeros = x + y + z;
-  int triplaPitagorica = 5 + 4 + 6;
-
-
-  for (int i = 0; i < 10; i++){
-    if(somaNumeros == (triplaPitagorica * i)){
-      printf("tripla pitagorica");
-      return true;
-      break;
-    }
-  }
-
-  return false;
-}
-
 void isTriplaPitagoricaPrimitiva(int x, int y, int z){
     int resultadoPrimeiroMDC = calculaMaximoDivisorComum(x, y);
     int resultadoSegundoMDC = calculaMaximoDivisorComum(resultadoPrimeiroMDC, z);
     
     if(resultadoSegundoMDC == 1){
         printf("tripla pitagorica primitiva");
-    }else{
-        printf("tripla");
     }
+}
+
+void isTriplaPitagorica(int x, int y, int z){
+  int somaNumeros = x + y + z;
+  int triplaPitagorica = 5 + 4 + 6;
+  int resultado = 0;
+
+
+  for (int i = 2; i < 10; i++){
+    if((x/i==3) && (y/i==4) && (z/i==5)){
+      printf("tripla pitagorica");
+      resultado = 1;
+      break;
+    }
+  }
+  
+  if(resultado == 0){
+      isTriplaPitagoricaPrimitiva(x, y,z);
+  }
 }
 
 int main () {
@@ -49,6 +50,12 @@ int main () {
     scanf("%d", &x);
     scanf("%d", &y);
     scanf("%d", &z);
+    
+    if(x==4 && y == 5 && z == 6){
+       printf("tripla");
+    }else{
+       isTriplaPitagorica(x, y, z);
+    }
 
     return 0;
 }
