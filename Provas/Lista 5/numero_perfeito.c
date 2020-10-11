10b046b7d484657bd80d8f05659fbe703b168720
franklaercio <franklaercio@gmail.com>
2020-10-10 23:05:36 -0300

    Adding numero_perfeito.c

--- Provas/Lista 5/numero_perfeito.c
+++ Provas/Lista 5/numero_perfeito.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int imprimirDivisores(int numero){
-    for(int i=0; i <= numero; i++){
-        if(numero%i == 0){
-            printf("%d", numero);
+int imprimeNumeroPerfeito(int numero){
+  printf("%i = 1", numero);
+
+  for(int i=2; i <= numero; i++){
+    if(numero % i == 0 && numero != i){
+      printf(" + %i", i);
+    }
+  }
+
+  return 0;
+}
+
+bool isNumeroPerfeito(int numero){
+    int resultado = 0;
+
+    for(int i=1; i <= numero; i++){
+        if(numero % i == 0 && numero != i){
+          resultado += i;
         }
     }
+
+    if (resultado == numero){
+      imprimeNumeroPerfeito(numero);
+    }else{
+      printf("%i nao eh perfeito", numero);
+    }
+
+    return 0;
 }
 
 int main () {
     int numero;
     scanf("%d", &numero);
 
-    imprimirDivisores(numero);
+    isNumeroPerfeito(numero);
 
     return 0;
 }
