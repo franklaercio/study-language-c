80c758e55ecff5d49fa8a5022e7820d1446dc8d3
franklaercio <franklaercio@gmail.com>
2020-10-20 00:14:37 -0300

    Adding recursive pi

--- Provas/Lista 6/pi_recursivo.c
+++ Provas/Lista 6/pi_recursivo.c
@@ -4,33 +4,43 @@
 
 double pi(int i){
   if(i == 0){
-    return 4 * pow(-1,i) / (2*i+1);
+    return 4 / ((2*i+1) * pow(-1, i));
   }
 
-  return (4 * pow(-1,i) / (2*i+1)) + pi(i - 1);
+  return (4 / ((2*i+1) * pow(-1, i)) + pi(i - 1));
+}
+
+double calcularPi(int p){
+  double valorPi = 0;
+  double piAnterior = 1;
+
+  for(int i = 0; i < 10001; i++){
+    if(((floor(valorPi*pow(10, p))) - (floor(piAnterior*pow(10, p)))) == 0){
+      break;
+    }
+
+    piAnterior = valorPi;
+    valorPi = pi(i);
+
+    if(i == 10000){
+      valorPi = -1;
+    }
+  }
+
+  return valorPi;
 }
 
 int main(){
     int p;
     char format[20];
-    int valorPi = 0;
-    int valorAnterior = 1;
-    int i = 0;
+    double pi = 0;
     
     scanf("%d", &p);
-
-    while(((valorPi * pow(10, p)) - (valorAnterior * pow(10, p))) != 0){
-        if(i == 10000){
-            valorPi = -1;
-        }
-        
-        valorAnterior = pi(i - 1);
-        valorPi = pi(i);
-        i++;
-    }
+    
+    pi = calcularPi(p);  
 
     sprintf(format, "Valor de pi %%.%dlf\n",p);
-    printf(format,valorPi);
+    printf(format,pi);
 
     return 0;
 }
