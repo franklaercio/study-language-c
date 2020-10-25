58ebb47b467a1d7475b955134e333e3187c163ac
Frank Laercio <franklaercio@gmail.com>
2020-10-25 20:15:23 -0300

    Adding questions

--- Provas/Lista 7/matriz_permutacao.c
+++ Provas/Lista 7/matriz_permutacao.c
@@ -1,5 +1,39 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+void preencherMatriz(int n, int m, int matriz[n][m]){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+}
 
 int main(){
+    int quadrado, contador = 0;
+    bool isMatrizPermutacao = true;
+
+    scanf("%i", &quadrado);
+    
+    int matriz[quadrado][quadrado];
+    
+    preencherMatriz(quadrado, quadrado, matriz);
+    
+    for(int i = 0; i < quadrado; i++){
+        for(int j = 0; j < quadrado; j++){         
+            if(matriz[i][j] == 1){
+                contador++;
+            }       
+        }
+        if(contador > 1){
+          isMatrizPermutacao = false;
+          break;
+        }else{
+          contador = 0;
+        }
+    }    
+
+    isMatrizPermutacao ? printf("1") : printf("0");
+    
     return 0;
 }
--- Provas/Lista 7/multiplicar_matrizes.c
+++ Provas/Lista 7/multiplicar_matrizes.c
@@ -1,5 +1,50 @@
 #include <stdio.h>
 
-int main(){
+void lerMatriz(int n, int m, int matriz[n][m]){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+}
+
+void imprimirMatriz(int n, int m,int matriz[n][m]){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            printf("%d ", matriz[i][j]);    
+        }
+        printf("\n");
+   }      
+}
+
+int main() {
+    int linhaA, colunaA, linhaB, colunaB, multiplicacao = 0;
+    
+    scanf("%i %i", &linhaA, &colunaA);
+    
+    int A[linhaA][colunaA];
+    lerMatriz(linhaA, colunaA, A);
+    
+    scanf("%i %i", &linhaB, &colunaB);
+    
+    int B[linhaB][colunaB];
+    lerMatriz(linhaB, colunaB, B);
+    
+    int C[linhaA][colunaB];
+    
+    for(int i = 0; i <= linhaA; i++) {
+        for(int j = 0; j <= colunaB; j++) {
+			for(int k = 0; k < linhaB; k++) {
+				multiplicacao +=  A[i][k] * B[k][j];
+			}
+
+			C[i][j] = multiplicacao;
+			multiplicacao = 0;
+        }
+        
+    }
+    
+    imprimirMatriz(linhaA, colunaB, C);
+    
     return 0;
 }
