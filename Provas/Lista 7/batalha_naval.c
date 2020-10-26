a3cce704eeb8cfa8353fadb6233ca959e5ea2d3f
Frank Laercio <franklaercio@gmail.com>
2020-10-25 23:43:10 -0300

    Adding batalha_naval and gomuko

--- Provas/Lista 7/batalha_naval.c
+++ Provas/Lista 7/batalha_naval.c
@@ -9,39 +9,85 @@ void lerCampoBatalha(int n, int m, int matriz[n][m]){
     }
 }
 
-void verificaTiro(int n, int m, int matriz[n][m], int tiroLinha, int tiroColuna){
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            if(matriz[i][j] != 0){
-                bool isCampoDireta = i + 1 < 4;
-                bool isCampoEsquerda = i - 1 < 4;
-                bool isCampoCima = m + j < 2*j;
-                bool isCampoBaixo = m + j < 2*j;
-
-                if(matriz[i][j]){
-                    printf("3\n");
-                }else{
-                    printf("2\n");
-                }
-            }else{
-                printf("1\n");
-            }
-        }
+int isPossivelVerificar(int testeLinha, int testeColuna, int linha, int coluna){
+    if(testeLinha > 3 || testeLinha < 0 || testeColuna > 3 || testeColuna < 0) {
+      return 0;
+    }else{
+      return 1;
     }
 }
 
+void verificaTiro(int n, int m, int matriz[n][m], int tiroLinha, int tiroColuna){
+    if(matriz[tiroLinha][tiroColuna] > 0){ 
+        int tiro = 0;
+        if(isPossivelVerificar(tiroLinha, tiroColuna + 1, n, m) == 1){
+            tiro += matriz[tiroLinha][tiroColuna + 1];
+        }
+
+        if(isPossivelVerificar(tiroLinha, tiroColuna - 1, n, m) == 1){
+            tiro += matriz[tiroLinha][tiroColuna - 1];
+        }
+
+        if(isPossivelVerificar(tiroLinha - 1, tiroColuna, n, m) == 1){
+            tiro += matriz[tiroLinha - 1][tiroColuna]; 
+        }
+        
+        if(isPossivelVerificar(tiroLinha + 1, tiroColuna, n, m) == 1){
+            tiro += matriz[tiroLinha + 1][tiroColuna];
+        }
+        
+        if(tiro == 0){
+            printf("3\n");
+        }else{
+            printf("2\n");
+        }
+
+        matriz[tiroLinha][tiroColuna] = 0;  
+    }else{
+        printf("1\n");
+    } 
+}
+
 int main(){
+    int tirosLinha[40];
+    int tirosColuna[40];
+    int salvaTiroLinha = 0, salvaTiroColuna = 0;
     int linha, coluna;
     scanf("%d %d", &linha, &coluna);
 
     int campo[linha][coluna];
     lerCampoBatalha(linha, coluna, campo);
 
-    int tiroLinha = 1, tiroColuna = 1;
+    int tiroLinha = 1, tiroColuna = 1, mesmoTiro = 0, tiros = 0;
 
-    while(tiroLinha != linha || tiroColuna != coluna){
+    while(true){
+        mesmoTiro = 0;
+        tiros = 0;
+        
         scanf("%d %d", &tiroLinha, &tiroColuna);
-        verificaTiro(linha, coluna, campo, tiroLinha, tiroColuna);
+        
+        if(((tiroLinha > 4) || (tiroLinha < 0)) || ((tiroColuna > 4) || (tiroColuna < 0))){
+          break;
+        }
+        
+        while(tiros < 40){
+            if(tirosLinha[tiros] == tiroLinha && tirosColuna[tiros] == tiroColuna){
+                mesmoTiro = 1;
+            }
+            tiros++;
+        }
+        
+        tirosLinha[salvaTiroLinha] = tiroLinha;
+        tirosColuna[salvaTiroColuna] = tiroLinha;
+        
+        salvaTiroLinha++;
+        salvaTiroColuna++;
+        
+        if(mesmoTiro == 0){
+            verificaTiro(linha, coluna, campo, tiroLinha - 1, tiroColuna - 1);
+        }else{
+            printf("4\n");
+        }
     }      
     
     return 0;
--- Provas/Lista 7/gomuko.c
+++ Provas/Lista 7/gomuko.c
@@ -1,5 +1,47 @@
 #include <stdio.h>
 
+void lerMatriz(int linha, int coluna, int matriz[linha][coluna]) {
+    for(int i = 0; i < linha; i++) {
+        for(int j = 0; j < coluna; j++) {
+            scanf("%d", &matriz[i][j]);
+        }
+        
+    }
+}
+
 int main(){
+    int matriz[15][15];
+    int direcao[4][2]={{1,0}, {0,1}, {1,1}, {1,-1}};
+    int peca = 0, resultado = 0, pecasSeguidas = 0, linha = 0, coluna = 0;
+    
+    lerMatriz(15, 15, matriz);
+    
+    for(int dir = 0; dir < 4; dir++){
+        for(int i = 0; i < 15; i++){
+            for(int j = 0; j < 15; j++){
+                peca = 0;
+                pecasSeguidas = 0;
+                
+                if(matriz[i][j] != 0){
+                    peca = matriz[i][j];
+                    linha = i, coluna = j;
+                    
+                	while(peca == matriz[linha][coluna]){
+                	    linha += direcao[dir][0];
+    	                coluna += direcao[dir][1];
+                	    pecasSeguidas++;
+                    }
+                    
+                    if(pecasSeguidas == 5){
+            	        resultado = peca;
+            	    }
+                }
+            }
+            
+        }
+    }
+    
+    printf("%d", resultado);
+    
     return 0;
 }
