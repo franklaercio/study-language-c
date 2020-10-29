f904cc35cb20ba515e93d5cf034d846da29c7ddc
Frank Laercio <franklaercio@gmail.com>
2020-10-28 23:01:40 -0300

    Adding matriz_ponteiro.c

---
+++ Listas/Semana 8/valor_maximo_minimo_ponteiro.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int tamanho, maximo, minimo, i_maximo, j_maximo, i_minimo, j_minimo;
+
+    scanf("%i", &tamanho);
+
+    int matriz[tamanho][tamanho];
+
+    return 0;
+}
