98940ba7b67bfc19be2572a297b046f6f526cb21
Frank Laercio <franklaercio@gmail.com>
2020-10-30 20:53:31 -0300

    Adding question

--- Provas/Lista 8/ordenacao_indireta.c
+++ Provas/Lista 8/ordenacao_indireta.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+void imprime_indireto(int **A_indireto, int n) {
+    for (int i=0; i<n; i++)
+        printf ("%d ", **(A_indireto+i));
+}
+
 int main() {
+    int tamanho;
+    scanf("%i", tamanho);
+
+    int A[tamanho];
+
+    imprime_indireto(&A, tamanho);
+
     return 0;
 }
