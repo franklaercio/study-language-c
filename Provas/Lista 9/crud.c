225bb6aa4e8c92c521cd55667576f09cfa39829c
Frank Laercio <franklaercio@gmail.com>
2020-11-09 02:37:39 -0300

    Update crud.c

--- Provas/Lista 9/crud.c
+++ Provas/Lista 9/crud.c
@@ -1,60 +1,40 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
-struct person {
+typedef struct person {
     char name[21];
-    char age[3];
+    int age;
     char gender;
-};
+} Person;
 
 struct person peaple[20];
 
-void create(int personId, char values[256]) {
-    char typedName[256];
-    char typedGender;
-    char typedAge[3];
-    int position, index = 0;
-
-    for (int i = 0; i < strlen(values); i++){
-        if(values[i] == ',') {
-            position = i;
-
-            for (int j = 0; j < position; j++){
-                typedName[j] = values[j];
-            }
-            
-            typedName[position] = '\0';
-            typedAge[0] = values[position + 1];
-            typedAge[1] = values[position + 2];
-            typedAge[2] = '\0';
-            typedGender = values[position + 4];
-
-            strcpy(peaple[personId].name, typedName);
-            strcpy(peaple[personId].age, typedAge);
-            peaple[personId].gender = typedGender;
-
-            break;
-        }
-    }
+void create(int personId, Person person) {
+    peaple[personId] = person;
 }
 
-void delete(char typedPerson[256]) {
-    for (int i = 0; i < strlen(typedPerson); i++){
-      if(typedPerson[i] == ',') {
-        for (int i = 0; i < 20; i++){
-          if (peaple[i].name[0] == typedPerson[0] && peaple[i].name[1] == typedPerson[1] && peaple[i].name[2] == typedPerson[2]) {
-            peaple[i].age[0] = 'X';
-            peaple[i].age[1] = 'X';
-          }
-        }
-      }  
+void delete(Person person) {
+    for (int i = 0; i < 20; i++){
+      bool isNameEquals = true;
+      
+      for(int j = 0; j < strlen(peaple[i].name); j++) {
+        if(peaple[i].name[j] != person.name[j]) {
+          isNameEquals = false;
+          break;
+        }  
+      }
+
+      if (isNameEquals && peaple[i].age == person.age && peaple[i].gender == person.gender) {
+        peaple[i].age = -1;
+      }
     }  
 }
 
 void show() {
     for (int i = 0; i < 20; i++){
-        if(peaple[i].age[0] != 'X' && strlen(peaple[i].age) > 0) {
-            printf("%s,%s,%c\n", peaple[i].name, peaple[i].age, peaple[i].gender);
+        if(peaple[i].age > 0) {
+            printf("%s,%i,%c\n", peaple[i].name, peaple[i].age, peaple[i].gender);
         }
     }
 }
@@ -65,21 +45,33 @@ int main() {
     while(1) {
         char instruction[100];
         char values[256];
-
+        Person person;
         fgets(instruction, 256, stdin);
 
         if(instruction[1] == 'n') {
+            char name[256];
+            int age;
+            char gender;
             fgets(values, 256, stdin);
             values[strcspn(values, "\n")] = 0;
+            sscanf(values, "%[^,],%d,%c", name, &age, &gender);
 
-            //printf("%s\n", values);
+            Person person = {.age = age, .gender = gender};
+            strcpy(person.name, name);
 
-            create(personId, values);
+            create(personId, person);
             personId++;
         }else if(instruction[0] == 'd') {
-            char typedPerson[256];
-            scanf("%s[^,]", typedPerson);
-            delete(typedPerson);
+            char name[256];
+            int age;
+            char gender;
+            fgets(values, 256, stdin);
+            values[strcspn(values, "\n")] = 0;
+            sscanf(values, "%[^,],%d,%c", name, &age, &gender);
+
+            Person person = {.age = age, .gender = gender};
+            strcpy(person.name, name);
+            delete(person);
         }else {
             show();
             break;
@@ -88,3 +80,4 @@ int main() {
 
     return 0;
 }
+
